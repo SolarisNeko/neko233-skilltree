f51962cd6712d949cf040ca946311e93841a00a6
SolarisNeko <1417015340@qq.com>
2023-05-05 23:54:45 +0800

    [dev] 1. [Add] Rank API

---
+++ skilltree-rank/skilltree-rank-api/src/main/java/com/neko233/skilltree/rank/RankData.java
@@ -0,0 +1,35 @@
+package com.neko233.skilltree.rank;
+
+/**
+ * 排行榜用户信息，提供基础信息：
+ * <code>
+ * 1. 用户 id : userId / userName
+ * 2. 分数。
+ * 3. 创建时间
+ * 排名数据, 不应该考虑排名, 排名是从【宏观角度】观察的一个维度
+ * </code>
+ *
+ * @param <UID> 用户标识
+ */
+public interface RankData<UID> {
+
+
+    /**
+     * @return userId / username
+     */
+    UID getId();
+
+
+    /**
+     * @return 得分
+     */
+    long getScore();
+
+
+    /**
+     * @return 创建时间戳, 毫秒
+     */
+    long createMs();
+
+
+}
