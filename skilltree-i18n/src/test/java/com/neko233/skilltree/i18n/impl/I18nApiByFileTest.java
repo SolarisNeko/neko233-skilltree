c8991f53050f59ffb49ed7e6882d672d35466567
SolarisNeko <1417015340@qq.com>
2023-06-11 17:53:33 +0800

    v0.0.7 1. [Add] update i18n

---
+++ skilltree-i18n/src/test/java/com/neko233/skilltree/i18n/impl/I18nApiByFileTest.java
@@ -0,0 +1,28 @@
+package com.neko233.skilltree.i18n.impl;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author SolarisNeko
+ * Date on 2023-06-11
+ */
+public class I18nApiByFileTest {
+
+    @Test
+    public void load() {
+    }
+
+    @Test
+    public void getAll() {
+    }
+
+    @Test
+    public void get() {
+    }
+
+    @Test
+    public void set() {
+    }
+}
